de8a789c4c83d7fdfd06eafa48d74d864a0686e2
sujanthapa01 <sujanthapa4001@gmail.com>
2024-03-02 14:00:38 +0530

    commit

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 require('dotenv').config();
 const express = require('express');
 const router = require('./router/router');
-const mongooseConnectionToDB  =  require('./connection')
-const {Testusers} = require('./models/model')
+const mongooseConnectionToDB  =  require('./connection');
+const {Testusers} = require('./models/model');
 
 const app = express();
-const port = process.env.PORT || 300;
+const port = process.env.PORT || 8000;
 app.use(express.json());
 app.use(express.urlencoded({extended :false}));
 // mongoose Database Connection 
-const mongooseConnection = process.env.MONGOOSE_CONNECTION;
+const mongooseConnection = process.env.MONGOOSE_CONNECTION;  //Add your mongo Db server
 mongooseConnectionToDB(mongooseConnection).then(() => console.log(`mongodb Connected!`))
 
 app.use('/',router);
 
-
-
 app.listen(port,()=>console.log(`port is listning at http://lochalhost:${port}`)); 
+
+module.exports = {port};
--- router/router.js
+++ router/router.js
@@ -1,6 +1,14 @@
+require('dotenv').config();
 const express = require('express');
 const router = express.Router();
 const { getAllUsersFromDB, handleUpadeUserById, getUserFromId, handleDeleteUser, handlenewUser } = require('../controller/controller');
+
+const port = process.env.PORT || 8000;
+
+router.get('/',(req,res)=>{
+    return res.status(200).end(`go to homepage to see all users! \n http://localhost:${port}/home`);
+})
+
 
 router.get('/home', getAllUsersFromDB);
 router.get('/home/:id', getUserFromId);
